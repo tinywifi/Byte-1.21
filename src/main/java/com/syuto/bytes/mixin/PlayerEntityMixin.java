b8107880196435e9e3352842e57c0fd8ba29d3ef
Syuto <110631856+SyutoBestCoder@users.noreply.github.com>
2024-12-13 22:22:36 +0000

    yes

--- src/main/java/com/syuto/bytes/mixin/PlayerEntityMixin.java
+++ src/main/java/com/syuto/bytes/mixin/PlayerEntityMixin.java
@@ -1,27 +1,39 @@
 package com.syuto.bytes.mixin;
 
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import com.syuto.bytes.module.Module;
 import com.syuto.bytes.module.ModuleManager;
 import com.syuto.bytes.module.impl.combat.KeepSprint;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import static com.syuto.bytes.Byte.mc;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
 
-    @WrapWithCondition(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
-    private boolean setVelocity(PlayerEntity instance, Vec3d vec3d) {
-        KeepSprint e = ModuleManager.getModule(KeepSprint.class);
-        return e != null && e.isEnabled();
+    @ModifyArgs(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setSprinting(Z)V"))
+    private void modifySprintingArgs(Args args) {
+        KeepSprint ks = ModuleManager.getModule(KeepSprint.class);
+        if (ks != null && ks.isEnabled()) {
+            args.set(0, true);
+        }
     }
 
-    @WrapWithCondition(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setSprinting(Z)V"))
-    private boolean setSprinting(PlayerEntity instance, boolean b) {
-        KeepSprint e = ModuleManager.getModule(KeepSprint.class);
-        return e != null && e.isEnabled();
+    @ModifyArgs(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
+    private void modifyVelocityArgs(Args args) {
+        KeepSprint ks = ModuleManager.getModule(KeepSprint.class);
+        if (ks != null && ks.isEnabled()) {
+            args.set(0, mc.player.getVelocity());
+        }
     }
+
+
 }
