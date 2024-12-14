5728f81816e3815a6682348fa8631c7ef1edd907
Syuto <110631856+SyutoBestCoder@users.noreply.github.com>
2024-12-14 13:24:03 +0000

    d
    d

--- src/main/kotlin/dev/blend/ThemeHandler.kt
+++ src/main/kotlin/dev/blend/ThemeHandler.kt
@@ -9,7 +9,7 @@ import java.awt.Color
 
 object ThemeHandler {
 
-    private val module = ModuleManager.getModule(ThemeModule::class.java)!!
+    private val module get() = ModuleManager.getModule(ThemeModule::class.java)!!
     private val background = SineOutAnimation(500)
     private val text = SineInAnimation(500)
     private val foreground = SineInAnimation(500)
@@ -42,20 +42,24 @@ object ThemeHandler {
 
     @JvmStatic
     fun getPrimary(): Color {
-        return module.accent.value
+        return module.accent.getValue()
     }
+
     @JvmStatic
     fun getBackground(): Color {
         return ColorUtil.mixColors(getBaseColor(), getPrimary(), 0.05)
     }
+
     @JvmStatic
     fun getTextColor(): Color {
         return Color(text.get().toInt(), text.get().toInt(), text.get().toInt())
     }
+
     @JvmStatic
     fun getContrast(): Color {
         return Color(foreground.get().toInt(), foreground.get().toInt(), foreground.get().toInt())
     }
+
     fun getBaseColor(): Color {
         return Color(background.get().toInt(), background.get().toInt(), background.get().toInt())
     }
